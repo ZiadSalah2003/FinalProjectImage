8560dcdbff6fd9f4fbccd195ebab7f6c9d7a41af
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-05-13 23:48:52 +0300

    feat: Add function to convert color image to grayscale using maximum component method

---
+++ ConvertToGrayDecomposing.m
@@ -0,0 +1,24 @@
+% Function Convert Color Image To GrayScale Image Take One parameter 
+function gray_img = ConvertToGrayDecomposing(img)
+    % Extract Red, Green, and Blue colors component to R, G,and B
+    R=img(:, :, 1); %Red Color
+    G=img(:, :, 2); %Green Color
+    B=img(:, :, 3); %Blue Color
+       
+    % Getting number of Rows And Columns from Color Images 
+    [M, N,~]=size(img);
+
+    % create image 2D with the same size
+    gray_img=zeros(M, N, 'uint8');
+
+     %using this equation from slides
+     %Gray = (max([Red,Green ,Blue]));
+    for i = 1:M
+        for j = 1:N
+            gray_img(i,j) = (max([R(i, j),G(i, j),B(i, j)]));
+        end
+    end
+gray_img = uint8(gray_img);
+figure,imshow(img),title('Original Image')
+figure,imshow(gray_img),title('Gray Image')
+end
